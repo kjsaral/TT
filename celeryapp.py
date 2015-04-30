b867866e016b5f1c3383590b86c4ccf6fbcb8d19
Gregor Müllegger <gregor@muellegger.de>
2015-04-30 21:25:17 +0200

    Giving instructions in celeryapp.py docstring.

--- celeryapp.py
+++ celeryapp.py
@@ -1,3 +1,21 @@
+"""
+This file sets up the environment for the Celery task queue.
+
+You *MUST* import this file when running the oTree project in order to set
+everything up. See the default ``wsgi.py`` and ``manage.py`` files for
+examples. It's enough to simply put the following line in your startup code::
+
+    import celeryapp
+
+You can adjust this file to configure celery in specific ways, but in general
+you can use these settings out of the box for local development or on Heroku.
+
+To run celery, execute::
+
+    ./otree celery worker --app=celeryapp:app --loglevel=INFO
+
+See http://www.celeryproject.org/ for more information.
+"""
 import os
 import otree.celery
 
