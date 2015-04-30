05d1a99704a45352daa4c4314c8772380e5de2e7
Gregor Müllegger <gregor@muellegger.de>
2015-04-30 21:05:06 +0200

    Importing celeryapp in wsgi to make celery work in deployments.

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 from django.core.wsgi import get_wsgi_application
 
+# Make sure the celery app is loaded.
+import celeryapp
+
 from dj_static import Cling
 application = Cling(get_wsgi_application())
